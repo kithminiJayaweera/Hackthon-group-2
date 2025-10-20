3ba1241c0fa9ceab0d57cfca4b564d223d3b2986
Raveesha Nethsarani <raveeshanethsarani963@gmail.com>
2025-10-20 13:36:42 +0530

    feat:added Innovation section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Footer from "./components/ui/Footer";
 import Hero from "./components/ui/Hero";
 import NavItem from "./components/navbar/navItem";
+import InnovationSection from "./components/ui/InnovationSection";
 
 function App() {
   return (
     <>
     <NavItem/>
       <Hero />
-      <NavItem />
+     <InnovationSection/>
       <Footer />
      
     </>
