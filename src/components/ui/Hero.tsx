d9853e15351dff002a68eb6022f53042782881e9
STNipun <imthisuranipun@gmail.com>
2025-10-20 13:45:09 +0530

    add: hero image

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,20 +1,24 @@
 
 import Stripe from "./Stripe";
+import HeroBackground from "../../assets/heroimage.jpg";
 
 const Hero = () => {
   return (
     <div className=" mx-auto min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-4xl md:text-5xl font-bold mb-4 text-white">
-        Work Smart, Earn Big, Learn Fast
-      </h1>
-      <p className="max-w-[700px] text-lg md:text-xl  text-white">
-        Experience a rewarding academic journey, personal growth, and a lively
-        social and sports community.
-      </p>
-    <button className=" rounded-4xl mt-6 px-6 py-3 bg-white text-black  hover:bg-blue-700 transition"> Apply Now </button>
-  
-    <Stripe />
-  
+      <img src={HeroBackground} alt="Hero Background" className="absolute inset-0 w-full h-screen object-cover" />
+      <div className="flex flex-col items-center justify-center z-10">
+        <h1 className="Shlop text-4xl tracking-[.20em] md:text-[80px] font-bold mb-4 text-white z-10">
+          Work Smart, Earn Big, Learn Fast
+        </h1>
+        <p className="max-w-[700px] text-lg md:text-xl  text-white z-10">
+          Experience a rewarding academic journey, personal growth, and a lively
+          social and sports community.
+        </p>
+        <button className=" rounded-4xl mt-6 px-6 py-3 bg-white text-black transition"> Apply Now </button>
+
+      </div>
+      <Stripe />
+
     </div>
   );
 };
