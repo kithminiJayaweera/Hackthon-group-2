5f47e531a458556ea7277fe3a22cbae6ad97966a
Adeepa-s <adeepa.s@botcalm.com>
2025-10-20 14:00:16 +0530

    feat added career haloween section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import NavItem from "./components/navbar/navItem";
+import Career from "./pages/CareerHalloween";
 
 function App() {
   return (
-    <NavItem/>
+    <>
+      {/* <NavItem /> */}
+      <Career />
+    </>
   );
 }
 
